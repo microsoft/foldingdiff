307ae3b584d228ed5995cf8d5798f5cd05a1bbfb
Kevin Wu <t-kevinwu@microsoft.com>
2022-07-01 22:52:05 +0000

    Add script to download cath data

---
+++ data/download_cath.sh
@@ -0,0 +1,5 @@
+# cd into data directory and execute shell script
+# md5 cbb61487871603181d5e38e1ebff486a
+wget -P cath http://people.csail.mit.edu/ingraham/graph-protein-design/data/cath/chain_set.jsonl
+# md5 0b84c1c522e51ebfe97e92e5cf0f36cc
+wget -P cath http://people.csail.mit.edu/ingraham/graph-protein-design/data/cath/chain_set_splits.json
