47197a7a1454c975791125e2deb840f7c960ed49
Kevin Wu <t-kevinwu@microsoft.com>
2022-07-17 18:05:39 +0000

    Angle loss

---
+++ protdiff/losses.py
@@ -0,0 +1,36 @@
+"""
+Loss functions!
+"""
+
+import numpy as np
+
+import torch
+from torch import nn
+
+
+def radian_l1_loss(input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    """
+    Computes the loss between input and target
+    >>> radian_l1_loss(torch.tensor(0.1), 2 * torch.pi)
+    tensor(0.1000)
+    >>> radian_l1_loss(torch.tensor(0.1), torch.tensor(2 * np.pi - 0.1))
+    tensor(0.2000)
+    """
+    # https://stackoverflow.com/questions/1878907/how-can-i-find-the-difference-between-two-angles
+    target = target % (2 * torch.pi)
+    input = input % (2 * torch.pi)
+    d = target - input
+    d = (d + torch.pi) % (2 * torch.pi) - torch.pi
+    return torch.abs(d)
+
+
+def main():
+    l = radian_l1_loss(torch.tensor(0.1), torch.tensor(2 * np.pi - 0.1))
+    print(l)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+    main()
