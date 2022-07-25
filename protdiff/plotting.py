b6a31422f2e85ad498fa5bde4052831fa8adc354
Kevin Wu <t-kevinwu@microsoft.com>
2022-07-25 22:37:13 +0000

    Centralize code for plotting vals

--- protdiff/plotting.py
+++ protdiff/plotting.py
@@ -1,9 +1,49 @@
 """
 Utility functions for plotting
 """
-
-
 import os
+
+from tqdm.auto import tqdm
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import torch
 
 PLOT_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "plots")
 assert os.path.isdir(PLOT_DIR)
+
+
+def plot_val_dists_at_t(
+    dset, t: int, share_axes: bool = True, zero_center_angles: bool = False, mod_2pi_angles:bool=True,
+):
+    select_by_attn = lambda x: x["corrupted"][torch.where(x["attn_mask"])]
+
+    retval = []
+    for i in tqdm(range(len(dset))):
+        vals = dset.__getitem__(i, use_t_val=t)
+        assert vals["t"].item() == t
+        retval.append(select_by_attn(vals))
+    vals_flat = torch.vstack(retval).numpy()
+    assert len(vals_flat.shape) == 2
+
+    fig, axes = plt.subplots(
+        2, 2, sharex=share_axes, sharey=share_axes, dpi=300, figsize=(9, 7)
+    )
+    for i, ax in enumerate(axes.flatten()):
+        val_name = ["dist", "omega", "theta", "phi"][i]
+        # Plot the values
+        vals = vals_flat[:, i]
+        if val_name != 'dist':
+            vals = vals % (2 * np.pi)
+        sns.histplot(vals, ax=ax)
+        if val_name != "dist":
+            if zero_center_angles:
+                ax.axvline(np.pi, color="tab:orange")
+                ax.axvline(-np.pi, color="tab:orange")
+            else:
+                ax.axvline(0, color="tab:orange")
+                ax.axvline(2 * np.pi, color="tab:orange")
+        ax.set(title=f"Timestep {t} - {val_name}")
+    return fig
