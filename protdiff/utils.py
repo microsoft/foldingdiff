04ed86692cdddb0470975e8b4c3bbf33d154877d
Kevin Wu <t-kevinwu@microsoft.com>
2022-07-27 18:21:37 +0000

    Efficient metohd for broadcasted mod

--- protdiff/utils.py
+++ protdiff/utils.py
@@ -33,6 +33,25 @@ def num_to_groups(num: int, divisor: int) -> List[int]:
     return arr
 
 
+def broadcast_mod(x: torch.Tensor, m: Union[float, torch.Tensor]) -> torch.Tensor:
+    """
+    Perform modulo on x % m while broadcasting
+    >>> broadcast_mod(torch.arange(24).reshape(2, 3, 4), torch.tensor([5, 7, 9, 11]))
+    tensor([[[0, 1, 2, 3],
+             [4, 5, 6, 7],
+             [3, 2, 1, 0]],
+    <BLANKLINE>
+            [[2, 6, 5, 4],
+             [1, 3, 0, 8],
+             [0, 0, 4, 1]]])
+    """
+    if isinstance(m, float):
+        return torch.remainder(x, m)
+    # m is a tensor so we need to broadcast
+    # https://pytorch.org/docs/stable/generated/torch.Tensor.expand.html#torch.Tensor.expand
+    return torch.remainder(x, m.expand(*x.shape))
+
+
 if __name__ == "__main__":
     import doctest
 
