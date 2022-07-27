e19d1335f5466ee7d2d39c8949bad0e6043c3d74
Kevin Wu <t-kevinwu@microsoft.com>
2022-07-27 21:31:52 +0000

    Script to get alphafold data

---
+++ data/download_alphafold.sh
@@ -0,0 +1,3 @@
+# Downloads the AlphaFold predictions for all of SwissProt as described
+# at https://alphafold.ebi.ac.uk/download#swissprot-section
+wget https://ftp.ebi.ac.uk/pub/databases/alphafold/latest/swissprot_pdb_v2.tar
