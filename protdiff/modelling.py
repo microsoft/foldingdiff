c8fcc4253ff8ac207807f9f809d6d6b87469af47
Kevin Wu <t-kevinwu@microsoft.com>
2022-08-12 21:11:45 +0000

    Implement option of deeper decoder

--- protdiff/modelling.py
+++ protdiff/modelling.py
@@ -19,8 +19,8 @@ from transformers import BertConfig
 from transformers.models.bert.modeling_bert import (
     BertPreTrainedModel,
     BertEncoder,
-    BertPooler,
 )
+from transformers.activations import get_activation
 
 import losses
 import utils
@@ -149,11 +149,52 @@ class BertEmbeddings(nn.Module):
         return embeddings
 
 
+class AnglesPredictor(nn.Module):
+    """
+    Predict angles from the embeddings. For BERT, the MLM task is done using an
+    architecture like
+    d_model -> dense -> d_model -> activation -> layernorm -> dense -> d_output
+    https://github.com/huggingface/transformers/blob/v4.21.1/src/transformers/models/bert/modeling_bert.py#L681
+
+    activation should be given as nn.ReLU for example -- NOT nn.ReLU()
+    """
+
+    def __init__(
+        self,
+        d_model: int,
+        d_out: int = 4,
+        activation: Union[str, nn.Module] = "gelu",
+        eps: float = 1e-12,
+    ) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.d_out = d_out
+        self.dense1 = nn.Linear(d_model, d_model)
+
+        if isinstance(activation, str):
+            self.dense1_act = get_activation(activation)
+        else:
+            self.dense1_act = activation()
+        self.layer_norm = nn.LayerNorm(d_model, eps=eps)
+
+        self.dense2 = nn.Linear(d_model, d_out)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.dense1(x)
+        x = self.dense1_act(x)
+        x = self.layer_norm(x)
+        x = self.dense2(x)
+        return x
+
+
 class BertForDiffusion(BertPreTrainedModel, pl.LightningModule):
     """
     BERT designed to be used with continuous inputs instead of tokens
 
     Reference: https://github.com/huggingface/transformers/blob/f681437203baa7671de3174b0fa583c349d9d5e1/src/transformers/models/bert/modeling_bert.py#L870
+
+    Decoder: linear = single linear decoding of per-position embeddings
+             mlp = two-layer MLP to decode per-position embeddings
     """
 
     loss_fn_dict = {
@@ -177,6 +218,7 @@ class BertForDiffusion(BertPreTrainedModel, pl.LightningModule):
         config,
         n_inputs: int = 4,
         time_encoding: Literal["gaussian_fourier", "sinusoidal"] = "sinusoidal",
+        decoder: Literal["linear", "mlp"] = "linear",
         lr: float = 1e-4,
         loss: Union[
             Callable, Literal["huber", "radian_l1", "radian_l1_smooth"]
@@ -218,7 +260,12 @@ class BertForDiffusion(BertPreTrainedModel, pl.LightningModule):
         self.encoder = BertEncoder(config)
 
         # Set up the network to project token representation to our four outputs
-        self.token_decoder = nn.Linear(config.hidden_size, n_inputs)
+        if decoder == "linear":
+            self.token_decoder = nn.Linear(config.hidden_size, n_inputs)
+        elif decoder == "mlp":
+            self.token_decoder = AnglesPredictor(config.hidden_size, n_inputs)
+        else:
+            raise ValueError(f"Unrecognized decoder: {decoder}")
 
         # Set up the time embedder
         if time_encoding == "gaussian_fourier":
