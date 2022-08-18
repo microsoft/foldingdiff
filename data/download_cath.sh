73b0c43ce2bf2bc76e1474402c156ca6b2c1ff8f
Kevin Wu <t-kevinwu@microsoft.com>
2022-08-18 14:01:29 -0700

    Update cath download script

--- data/download_cath.sh
+++ data/download_cath.sh
@@ -3,3 +3,4 @@
 wget -P cath http://people.csail.mit.edu/ingraham/graph-protein-design/data/cath/chain_set.jsonl
 # md5 0b84c1c522e51ebfe97e92e5cf0f36cc
 wget -P cath http://people.csail.mit.edu/ingraham/graph-protein-design/data/cath/chain_set_splits.json
+gzip cath/chain_set.jsonl
