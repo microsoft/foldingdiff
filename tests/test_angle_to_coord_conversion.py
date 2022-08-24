1881f833c412480e5a6088ab48bd21968edfe4f5
Kevin Wu <t-kevinwu@microsoft.com>
2022-08-24 23:39:53 +0000

    Add failing test. Dihedral reversal not really workingz

--- tests/test_angle_to_coord_conversion.py
+++ tests/test_angle_to_coord_conversion.py
@@ -73,5 +73,19 @@ class TestDihedralReversalEasy(unittest.TestCase):
         )
 
 
+class TestDihedralReversalRandom(unittest.TestCase):
+    def test_random(self):
+        rng = np.random.default_rng(seed=6489)
+        for _ in range(10):
+            a, b, c, d = rng.random((4, 1, 3))
+            dh = pdb_utils.get_dihedrals(a, b, c, d)
+            expected = d - c
+            expected /= np.linalg.norm(expected)
+            rev = angles_to_coords.reverse_dihedral(a, b, c, dh)
+            self.assertTrue(
+                np.allclose(expected, rev), msg="{} != {}".format(expected, rev)
+            )
+
+
 if __name__ == "__main__":
     unittest.main()
