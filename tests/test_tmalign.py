d2f6a45f4e9610d3998fdf9818bc228118062c1a
Kevin Wu <t-kevinwu@microsoft.com>
2022-09-02 14:13:51 -0700

    Basic tests for tmalign wrapper

---
+++ tests/test_tmalign.py
@@ -0,0 +1,49 @@
+"""
+Test the TMalign wrapper
+"""
+
+import os, sys
+import unittest
+
+import numpy as np
+
+SRC_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "protdiff")
+assert os.path.isdir(SRC_DIR)
+sys.path.append(SRC_DIR)
+
+import tmalign
+
+
+class TestTMalign(unittest.TestCase):
+    """
+    Test the TMalign wrapper
+    """
+
+    def setUp(self) -> None:
+        self.fname1 = os.path.join(
+            os.path.dirname(os.path.dirname(__file__)), "data", "7PFL.pdb"
+        )
+        self.fname2 = os.path.join(
+            os.path.dirname(os.path.dirname(__file__)), "data", "7ZYA.pdb"
+        )
+        assert os.path.isfile(self.fname1)
+        assert os.path.isfile(self.fname2)
+
+    def test_reproducibility(self):
+        """Test that the same file run twice gives consistent results"""
+        x = tmalign.run_tmalign(self.fname1, self.fname2)
+        y = tmalign.run_tmalign(self.fname1, self.fname2)
+        self.assertAlmostEqual(x, y)
+
+    def test_self_is_1(self):
+        """Test that comparing a file to itself should yield a perfect score of 1"""
+        x = tmalign.run_tmalign(self.fname1, self.fname1)
+        self.assertAlmostEqual(x, 1.0)
+    
+    def test_nonself_less_than_1(self):
+        """Test that comparing against a different structure yields a value < 1"""
+        x = tmalign.run_tmalign(self.fname1, self.fname2)
+        self.assertLess(x, 1.0)
+
+if __name__ == "__main__":
+    unittest.main()
