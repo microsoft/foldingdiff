6f32e0dd431a939061feee6fbd09468a93d55fbd
Kevin Wu <t-kevinwu@microsoft.com>
2022-09-08 15:03:35 -0700

    Fix cath download scripts

--- data/download_cath.sh
+++ data/download_cath.sh
@@ -3,8 +3,13 @@
 wget -P cath http://people.csail.mit.edu/ingraham/graph-protein-design/data/cath/chain_set.jsonl
 # md5 0b84c1c522e51ebfe97e92e5cf0f36cc
 wget -P cath http://people.csail.mit.edu/ingraham/graph-protein-design/data/cath/chain_set_splits.json
-gzip cath/chain_set.jsonl
 
 # download the source files
-wget -P ftp://orengoftp.biochem.ucl.ac.uk/cath/releases/latest-release/non-redundant-data-sets/cath-dataset-nonredundant-S40.pdb.tgz
+wget -P cath ftp://orengoftp.biochem.ucl.ac.uk/cath/releases/latest-release/non-redundant-data-sets/cath-dataset-nonredundant-S40.pdb.tgz
 
+# Check md5sums
+md5sum -c cath_checksums.md5 cath/
+
+# cd into the cath directory and untar the file
+cd cath
+tar -xzf cath-dataset-nonredundant-S40.pdb.tgz
