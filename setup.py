b1ee691a89422ba321e50403d84cea41a2f5d896
Kevin Wu <wukevin@stanford.edu>
2022-10-05 15:08:04 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,13 @@ with open("README.md", "r") as source:
 
 setup(
     name="foldingdiff",
+    author="Kevin Wu",
     packages=find_packages("foldingdiff"),
+    include_package_data=True,
     description="Diffusion for protein backbone generation using internal angles",
     long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/microsoft/foldingdiff",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
