6b98e00ddca71aacbdfca9d38a4dd405634a39b1
Kevin Wu <wukevin@stanford.edu>
2022-10-30 15:51:33 -0700

    Loss on pairwise distances given 3D coordinates are starting inputs

--- foldingdiff/losses.py
+++ foldingdiff/losses.py
@@ -1,13 +1,16 @@
 """
 Loss functions!
 """
-
+from typing import Optional, Sequence, Tuple
 import numpy as np
 
 import torch
 from torch import nn
+from torch.nn import functional as F
 
-from . import utils
+# from torchmetrics.functional import pairwise_euclidean_distance
+
+from foldingdiff import utils
 
 
 def radian_l1_loss(input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
@@ -61,8 +64,84 @@ def radian_smooth_l1_loss(
     return retval
 
 
+def _get_pairwise_dist_batch(
+    values: torch.Tensor, lengths: Sequence[int]
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Calculates the pairwise distance matrix for the given values
+    Returns two tensors of shape (batch, M) where M is the number of pairwise
+    distances, padded. First tensor is actual distances, second tensor is a mask.
+    Mask is formatted such that valid values are 1 and invalid values are 0.
+    """
+    assert values.ndim == 3, f"Expected 3D tensor of (batch, N, 3), got {values.ndim}D"
+    assert (
+        values.shape[-1] == 3
+    ), f"Expected 3D tensor of (batch, N, 3), got {values.shape}"
+    assert lengths.ndim == 1
+    assert values.shape[0] == lengths.shape[0]
+
+    # Calculate the pairwise distances
+    values = [F.pdist(values[i, :l]) for i, l in enumerate(lengths)]
+
+    # Pad
+    lengths = [len(v) for v in values]
+    max_len = max(lengths)
+    mask = torch.zeros((len(values), max_len))
+    for i, l in enumerate(lengths):
+        mask[i, :l] = 1.0
+
+    retval = torch.zeros((len(values), max_len))
+    for i, v in enumerate(values):
+        retval[i, : len(v)] = v
+    assert retval.shape == mask.shape
+
+    return retval, mask
+
+
+def pairwise_dist_loss(
+    input: torch.Tensor, target: torch.Tensor, lengths: Optional[torch.Tensor] = None
+):
+    """
+    Calculates the pairwise distance matrix for both the input and the target,
+    which are given in 3D cartesian coordinates of the shape (batch, N, 3) and
+    calculates a loss based on the difference in the pairwise distances. Padding
+    is handled using the lengths tensor, which is a 1D tensor of shape (batch,)
+    and contains the number of valid values in each batch. Pairwise distances are
+    calculated betwen all given coordinates.
+
+    Note that since we are comparing pairwise distances, this loss function itself
+    is rotation and shift invariant.
+    """
+    if lengths is None:
+        lengths = torch.IntTensor(
+            [
+                torch.all(~torch.isnan(input[i]), dim=1).sum()
+                for i in range(input.shape[0])
+            ]
+        )
+    assert lengths.shape[0] == input.shape[0]
+    # Calculate pairwise distances; these return (batch, M) tensors
+    # of the distances and a mask (where 0 indicates a padding value)
+    input_dists, input_mask = _get_pairwise_dist_batch(input, lengths)
+    target_dists, target_mask = _get_pairwise_dist_batch(target, lengths)
+    print(input_mask)
+
+    # Get the loss
+    # https://pytorch.org/docs/stable/generated/torch.nn.functional.mse_loss.html
+    loss = F.mse_loss(
+        input_dists[torch.where(input_mask)],
+        target_dists[torch.where(target_mask)],
+        reduction="mean",
+    )
+    return loss
+
+
 def main():
-    l = radian_smooth_l1_loss(torch.tensor(-17.0466), torch.tensor(-1.3888), beta=0.1)
+    lengths = torch.randint(2, 5, size=(16,)) * 3
+    x = torch.randn(16, 12, 3)
+    y = torch.randn(16, 12, 3)
+
+    l = pairwise_dist_loss(x, y, lengths)
     print(l)
 
 
