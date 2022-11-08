d1ccb7477f067f35000494c7dc6c222e11617e54
Kevin Wu <wukevin@stanford.edu>
2022-11-07 22:23:01 -0800

    Additional tests

--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -22,9 +22,16 @@ class TestSamplingReproducible(unittest.TestCase):
         self.full_model = "wukevin/foldingdiff"
 
     def test_repro_simple(self):
+        """Test that sampling from the same random seed produces the same result"""
         torch.manual_seed(1234)
         samp_1 = sampling.sample_simple(self.full_model, n=1, sweep_lengths=[50, 51]).pop()
         torch.manual_seed(1234)
         samp_2 = sampling.sample_simple(self.full_model, n=1, sweep_lengths=[50, 51]).pop()
         self.assertTrue(np.allclose(samp_1.values, samp_2.values))
-    
+
+    def test_diff_seed_diff_result(self):
+        """Test that having a different random state produces different results"""
+        torch.manual_seed(1234)
+        samp_1 = sampling.sample_simple(self.full_model, n=1, sweep_lengths=[50, 51]).pop()
+        samp_2 = sampling.sample_simple(self.full_model, n=1, sweep_lengths=[50, 51]).pop()
+        self.assertFalse(np.allclose(samp_1.values, samp_2.values))
