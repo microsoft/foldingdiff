658de68a49979cb21a627ab3bb440f467a824661
Kevin Wu <wukevin@stanford.edu>
2022-11-23 21:38:46 -0800

    Fix cath download script cosmetic error

--- data/download_cath.sh
+++ data/download_cath.sh
@@ -2,7 +2,7 @@
 wget -P cath ftp://orengoftp.biochem.ucl.ac.uk/cath/releases/latest-release/non-redundant-data-sets/cath-dataset-nonredundant-S40.pdb.tgz
 
 # Check md5sums
-md5sum -c cath_checksums.md5 cath/*
+md5sum -c cath_checksums.md5
 
 # cd into the cath directory and untar the file
 cd cath
