0aa1040f7ff3f44009ee6579d2d2677c1d3450c0
Kevin Wu <wukevin@stanford.edu>
2022-11-28 14:51:41 -0800

    Fix alphafold script to download v4

--- data/download_alphafold.sh
+++ data/download_alphafold.sh
@@ -1,3 +1,3 @@
 # Downloads the AlphaFold predictions for all of SwissProt as described
-# at https://alphafold.ebi.ac.uk/download#swissprot-section
-wget https://ftp.ebi.ac.uk/pub/databases/alphafold/latest/swissprot_pdb_v2.tar
+# Requires aria2c to be installed, see https://aria2.github.io
+aria2c -x10 https://ftp.ebi.ac.uk/pub/databases/alphafold/latest/swissprot_pdb_v4.tar
